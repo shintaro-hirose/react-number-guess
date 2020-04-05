28ea1a0ccc1148f5b0c0cc875a21fa7a742f1ef1
shintaro-hirose <joeleshin@eis.hokudai.ac.jp>
2020-04-05 13:58:24 +0900

    Updates

---
+++ precache-manifest.d78720cb1c634b469a17d0cdcd952cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53476fc8217aabc392a5be9cafc42780",
+    "url": "/react-number-guess/index.html"
+  },
+  {
+    "revision": "622b9e71077d7dd79685",
+    "url": "/react-number-guess/static/css/main.9b3a030e.chunk.css"
+  },
+  {
+    "revision": "11e47a780ba09fa01e7d",
+    "url": "/react-number-guess/static/js/2.416b8442.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-number-guess/static/js/2.416b8442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622b9e71077d7dd79685",
+    "url": "/react-number-guess/static/js/main.1e8ca050.chunk.js"
+  },
+  {
+    "revision": "6e0a5063cff6da91e55c",
+    "url": "/react-number-guess/static/js/runtime-main.32e6c1ca.js"
+  }
+]);
